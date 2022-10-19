09540ff0d5286e5c34e3a548769473dba4fbbc88
Oleg <oleg.ivn.irkhin@gmail.com>
2022-10-19 10:40:33 +0300

    Task 5

--- src/ru/mirea/task5/Main.java
+++ src/ru/mirea/task5/Main.java
@@ -1,24 +1,103 @@
 package ru.mirea.task5;
 import java.awt.*;
 import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 import javax.swing.*;
 
 public class Main {
+
+    static int madrid_goal = 0;
+    static int milan_goal = 0;
+
     public static void main(String[] args) {
         JFrame frame = new JFrame("Match");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(200,150);
+        frame.setSize(600,350);
         JPanel panel = new JPanel();
-        panel.setBackground(Color.BLACK);
+        panel.setBackground(Color.GRAY);
         panel.setPreferredSize(new Dimension(200, 300));
-        JButton milan = new JButton("AC Milan");
-        ActionListener actionListener = new Milan();
-        milan.addActionListener(actionListener);
-        frame.getContentPane().add(milan);
 
-        JButton madrid = new JButton("Madrid");
-        ActionListener actionListener_1 = new Madrid();
-        madrid.addActionListener(actionListener_1);
-        frame.getContentPane().add(madrid);
+
+        JLabel name_count = new JLabel("Madrid  AC Milan");
+        name_count.setBounds(690,10,500,100);
+        name_count.setFont(new Font("Arial", Font.PLAIN, 25));
+        name_count.setForeground(Color.BLUE);
+        frame.add(name_count);
+
+        JLabel count = new JLabel(madrid_goal + ":" + milan_goal);
+        count.setBounds(750,40,100,100);
+        count.setFont(new Font("Arial", Font.PLAIN, 25));
+        frame.add(count);
+
+        JLabel winner = new JLabel("Winner N/A");
+        winner.setBounds(710,100,500,100);
+        winner.setFont(new Font("Arial", Font.PLAIN, 25));
+        winner.setForeground(Color.RED);
+        frame.add(winner);
+
+        JButton madridButton = new JButton("Madrid");
+        JButton milanButton = new JButton("AC Milan");
+        JButton reset = new JButton("RESET");
+
+        madridButton.setBounds(550,200,150,80);
+        madridButton.setFont(new Font("Arial", Font.PLAIN, 25));
+        madridButton.setBackground(Color.ORANGE);
+        frame.add(madridButton);
+
+        milanButton.setBounds(900,200,150,80);
+        milanButton.setFont(new Font("Arial", Font.PLAIN, 25));
+        milanButton.setBackground(Color.GREEN);
+        frame.add(milanButton);
+
+        reset.setBounds(725,200,150,80);
+        reset.setFont(new Font("Arial", Font.PLAIN, 25));
+        reset.setBackground(Color.GRAY);
+        frame.add(reset);
+
+        madridButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                madrid_goal++;
+                count.setText(madrid_goal + ":" + milan_goal);
+                if (madrid_goal > milan_goal) {
+                    winner.setText("Winner: Madrid");
+                } else {
+                    if (milan_goal > madrid_goal) {
+                        winner.setText("Winner: AC Milan");
+                    } else {
+                        winner.setText("Winner: Draw");
+                    }
+                }
+            }
+        });
+
+        milanButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                milan_goal++;
+                count.setText(madrid_goal + ":" + milan_goal);
+                if (madrid_goal > milan_goal) {
+                    winner.setText("Winner: Madrid");
+                } else {
+                    if (milan_goal > madrid_goal) {
+                        winner.setText("Winner: AC Milan");
+                    } else {
+                        winner.setText("Winner: Draw");
+                    }
+                }
+            }
+        });
+
+        reset.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                madrid_goal = 0;
+                milan_goal = 0;
+                count.setText(madrid_goal + ":" + milan_goal);
+                winner.setText("Winner: N/A");
+            }
+        });
+
+
+        frame.getContentPane().add(panel);
+        frame.pack();
+        frame.setVisible(true);
     }
 }
